a6ce96a3580efa881f23008d43b5bb46b6eaee9c
unknown <urigopar889@gmail.com>
2023-02-16 09:47:42 -0600

    Variables y Consola JavaScript

---
+++ java.js
@@ -0,0 +1,15 @@
+/*Var es para variables como también con let */
+var comida="Chilaquiles";
+let cena="tacos";
+
+let precio=70;
+
+console.log("Hola");
+console.info("valor  de la comida "+ comida);
+console.warn("El precio se puede modificar");
+console.error("Te dije que el precio no se puede modificar")
+
+console.assert(1 === 1);
+console.assert(1 === "1");
+console.assert(1 == "1");
+console.assert( 1 == true);
