e440524736f59e01494c9cea3f58fc0306352f38
unknown <urigopar889@gmail.com>
2023-02-17 19:26:46 -0600

    Ciclo for java

---
+++ Lab_4.js
@@ -0,0 +1,9 @@
+let num= prompt("Hola, dame un número");
+for(let i=1; i <=num; i++){
+console.log(`iteracion ${i}`);
+v=i*i
+console.log(`iteracion ${v}`);
+f=i*i*i
+console.log(`iteracion ${f}`)
+}
+console.log("Fin")
