790b1843fbe93efda6f80f0883f1128564268f3f
unknown <urigopar889@gmail.com>
2023-02-18 12:23:32 -0600

    Arreglo encontrar psoitivos, negativos y ceros

--- Lab_4.js
+++ Lab_4.js
@@ -21,4 +21,45 @@ if(res == r){
 }else{
     document.write("Su respuesta es incorrecta :(");
 }
+//Falta saber el tiempo que tarda en ejecutar
 
+//Ejercicio 3 Contador
+/*
+let u=0;
+let arreglo =[ 0,0,-1,-2,3,4];
+for(let n=0;n<=arreglo.length;n++){
+    if(arreglo == 0){
+        u++
+    }
+    console.log(arreglo[u]);
+}*/
+/*
+const negativos="Cantidad de valores <0 ";
+const cero="Cantidad de valores <0 ";
+const positivos="Cantidad de valores <0 ";
+const numeros={};
+numeros[negativos]=0;
+numeros[cero]=0;
+numeros[positivos]=0;
+*/
+let numerosn=0;
+let numerosc=0;
+let numerosp=0;
+let arreglo =[ 0,,-1,-2,3,4];
+for (let n= 0;n<arreglo.length;n++){
+    if(arreglo[n]<0){
+        numerosn=numerosn+1;
+        console.log(numerosn);
+    }
+    if (arreglo[n]==0){
+        numerosc=numerosc+1;
+        console.log(numerosc);
+    }
+    if(arreglo[n]>0){
+    numerosp=numerosp+1;
+    console.log(numerosp);
+    }
+}
+document.write("Numeros en negativos son: "+numerosn);
+document.write("Numeros en cero son: "+numerosc);
+document.write("Numeros en positivos son: "+numerosp);
