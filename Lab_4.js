99cdd8ca956a3c3b2f1f2841f38ec5b01f2f40c6
unknown <urigopar889@gmail.com>
2023-02-17 20:32:58 -0600

    Suma 2 numeros aleatorios

--- Lab_4.js
+++ Lab_4.js
@@ -6,4 +6,12 @@ console.log(`iteracion ${v}`);
 f=i*i*i
 console.log(`iteracion ${f}`)
 }
-console.log("Fin")
+console.log("Fin");
+
+
+let numero1= Math.floor(Math.random()*100);
+let numero2= Math.floor(Math.random()*100);
+let res = prompt("Dame la suma de " + numero1+ " + " + numero2);
+
+
+
