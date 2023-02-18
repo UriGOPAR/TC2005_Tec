5be4eb3500390f19b5deb77635cd15ad928cd0f7
unknown <urigopar889@gmail.com>
2023-02-17 20:46:40 -0600

    Ciclo If Java

--- Lab_4.js
+++ Lab_4.js
@@ -1,3 +1,4 @@
+//Ejercicio numero dado y sus cuadrados y cubos 
 let num= prompt("Hola, dame un número");
 for(let i=1; i <=num; i++){
 console.log(`iteracion ${i}`);
@@ -8,10 +9,16 @@ console.log(`iteracion ${f}`)
 }
 console.log("Fin");
 
-
+//Ejercicio 2 suma de dos núemros aleatorios 
 let numero1= Math.floor(Math.random()*100);
 let numero2= Math.floor(Math.random()*100);
 let res = prompt("Dame la suma de " + numero1+ " + " + numero2);
 
+let r=parseInt(numero1)+parseInt(numero2);
 
+if(res == r){
+    document.write("Su respuesta es correcta :)");1
+}else{
+    document.write("Su respuesta es incorrecta :(");
+}
 
