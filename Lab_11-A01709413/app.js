2f69516ce4ea4e48fe6d0dff10e592a595360d70
unknown <urigopar889@gmail.com>
2023-02-28 10:35:34 -0600

    Creacion de formulario y devuelva una string

--- Lab_11-A01709413/app.js
+++ Lab_11-A01709413/app.js
@@ -1,6 +1,9 @@
-const { request, response } = require('express');
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 //Middleware
 app.use((request, response, next) => {
@@ -11,16 +14,35 @@ app.use((request, response, next) => {
 app.use((request, response, next) => {
     console.log('Otro middleware!');
     next();
-
 });
+
 app.use('/hola', (request, response, next) => {
-    response.send("Hola desde la ruta '/hola");
+    response.send('Hola desde la ruta /hola');
 });
 
-app.use( (request, response, next) => {
+app.get('/nuevo', (request, response, next) => {
+    
+    let html = `
+        <form action="nuevo" method="POST">
+        <label for="jugador">Nombre del jugador:</label>
+        <input type="text" id="jugador" name="jugador">
+        <input type="submit" value="Enviar">
+        </form>
+    `;
+    response.send(html);
+});
+
+app.post('/nuevo', (request, response, next) => {
+    console.log(request.body);
+    console.log(request.body.jugador);
+    response.send("El jugador es: " + request.body.jugador);
+});
+
+app.use((request, response, next) => {
     console.log("Tercer middleware");
 
     //Envía la respuesta al cliente
-    response.send("Hola desde el tercer middleware");
+    response.send('Hola desde el tercer middleware');
 });
+
 app.listen(3000);
