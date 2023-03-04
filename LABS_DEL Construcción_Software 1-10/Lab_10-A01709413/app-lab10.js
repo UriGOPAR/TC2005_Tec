b2d054c12ae967505452c6e422e65966b1248998
unknown <urigopar889@gmail.com>
2023-03-04 10:09:08 -0600

    Creación de diversas rutas con http

--- LABS_DEL Construcción_Software 1-10/Lab_10-A01709413/app-lab10.js
+++ LABS_DEL Construcción_Software 1-10/Lab_10-A01709413/app-lab10.js
@@ -7,10 +7,94 @@ const server =http.createServer((request,response) => {
         response.write('<!DOCTYPE html>');
         response.write('<html>');
         response.write('<head><meta charset="utf-8"></head><body>');
-        response.write("<h1>Chilaquiles</h1>");
-        response.write('<a href="/ordenar">Ordena tus chilaquiles aquí</a>');
+        response.write("<h1>Perritos</h1>");
+        response.write('<a href="/adoptar">Adoptar al perrito</a>');
+        response.end();
+
+    }   else if (request.url === "/adoptar" && request.method === "GET") {
+        response.setHeader('Content-Type', 'text/html');
+        response.write('<!DOCTYPE html>');
+        response.write('<html>');
+        response.write('<head><meta charset="utf-8"></head><body>');
+        response.write("<h1>Perritos</h1>");
+        response.write('<form action="/adoptar" method="POST">');
+
+        let radios = '<fieldset><legend>¿Cuál perrito te gustaría adoptar</legend>';
+        radios += '<div><input type="radio" id="labrador" name="razas" value="labrador" checked>'
+        radios += '<label for="labrador">Labrador</label></div>';
+        radios += '<div><input type="radio" id="pug" name="razas" value="pug">';
+        radios += '<label for="pug">Pug</label></div>';
+        radios += '<div><input type="radio" id="doberman" name="razas" value="doberman">';
+        radios += '<label for="doberman">Doberman</label></div>';
+        radios += '<div><input type="radio" id="chihuahua" name="razas" value="chihuahua">';
+        radios += '<label for="chihuahua">Chihuahua</label></div></fieldset><br>';
+        //
+        response.write(radios);
+
+        response.write('<label for="extras">Nombre: </label>');
+        response.write('<input type="text" id="info" name="info"><br><br>');
+        response.write('<label for="extras">Escribe tu domicilio: </label>');
+        response.write('<input type="text" id="info" name="info"><br><br>');
+        response.write('<label for="extras">Número de télefono: </label>');
+        response.write('<input type="text" id="info" name="info"><br><br>');
+
+        response.write('<input type="submit" value="Adoptar">');
+
+        response.write("</form>");
+        response.write('</body></html>');
+        response.end();
+    }else if (request.url === "/adoptar" && request.method === "POST") {
+
+        const datos = [];
+
+        request.on('data', (dato) => {
+            console.log(dato);
+            datos.push(dato);
+        });
+
+        return request.on('end', () => {
+            const datos_completos = Buffer.concat(datos).toString();
+            console.log(datos_completos);
+            const razas = datos_completos.split('&')[0].split('=')[1];
+            console.log(razas);
+            if(razas === "labrador") {
+                response.setHeader('Content-Type', 'text/html');
+                response.write('<!DOCTYPE html>');
+                response.write("Gracias por tu adoptar :)");
+                response.write('<img alt="Labrador" src="https://i.pinimg.com/originals/99/f9/ed/99f9ede31328c8484e9e252d08811535.jpg">');
+                return response.end();
+
+            } else if (razas == "pug"){
+                response.setHeader('Content-Type', 'text/html');
+                response.write('<!DOCTYPE html>');
+                response.write("Gracias por adoptar :)");
+                response.write('<img alt="Pug" src="https://animalia-life.com/data_images/pug/pug6.jpg">');
+                return response.end();
+
+            } else if (razas == "doberman"){
+                response.setHeader('Content-Type', 'text/html');
+                response.write('<!DOCTYPE html>');
+                response.write("Gracias por adoptar :)");
+                response.write('<img alt="Doberman" src="https://notasdemascotas.com/wp-content/uploads/2017/02/Doberman-cachorro.jpg">');
+                return response.end();
+            }else {
+                response.setHeader('Content-Type', 'text/html');
+                response.write('<!DOCTYPE html>');
+                response.write("Gracias por adoptar :)");
+                response.write('<img alt="Chihuahua" src="https://i.ytimg.com/vi/124ouDf3Dg0/maxresdefault.jpg">');
+                return response.end();
+            }
+            
+        });
+    }else if(request.url ){
+
+        
+    } else {
+        response.statusCode = 404;
+        response.write("Lo sentimos, de esos chilaquiles no tenemos");
         response.end();
     }
+
 });
 server.listen(3000);
 /*const server = http.createServer( (request, response) => {    
