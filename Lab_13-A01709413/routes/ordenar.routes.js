b0e57f76ca12a266dfecd739f8e5413bc504e93e
unknown <urigopar889@gmail.com>
2023-03-11 20:35:33 -0600

    Creación de models_ordenar y archivos ejs

--- Lab_13-A01709413/routes/ordenar.routes.js
+++ Lab_13-A01709413/routes/ordenar.routes.js
@@ -5,8 +5,16 @@ const filesystem =require ('fs');
 const ordenarController = require('../controllers/ordenar.controller');
 
 router.get('/pedir', ordenarController.getPedir);
+
 router.get('/adoptar', ordenarController.getAdoptar);
+
+router.get('/registro', ordenarController.getRegistro);
+
+router.post('/registro', ordenarController.postRegistro);
+
 router.get('/datos', ordenarController.getDatos);
+
 router.post('/datos', ordenarController.postDatos);
+
 
 module.exports=router;
