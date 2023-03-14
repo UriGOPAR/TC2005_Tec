d50911f979c12cfb7be85f740b02f4ee95ad67a5
unknown <urigopar889@gmail.com>
2023-03-14 10:15:48 -0600

    Csurf autetificacion

--- Notas de clase/app.js
+++ Notas de clase/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const session = require('express-session');
+const csrf = require('csurf');
 const isAuth = require('./util/is-auth');
 
 const app = express();
@@ -18,6 +19,14 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+
+//CSRF Protection
+const csrfProtection = csrf();
+app.use(csrfProtection); 
+app.use((request, response, next) => {
+    response.locals.csrfToken = request.csrfToken();
+    next();
+});
 
 //Middleware
 app.use((request, response, next) => {
