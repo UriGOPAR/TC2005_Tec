e27de98753879a989d2ffc305281d06601147ac7
unknown <urigopar889@gmail.com>
2023-03-19 22:09:13 -0600

    Lab_19

---
+++ Lab_19-A01709413/routes/peliculas.routes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const router = express.Router();
+
+const peliculasController = require('../controllers/peliculas.controller');
+
+router.get('/nueva', peliculasController.get_nueva);
+
+router.post('/nueva', peliculasController.post_nueva);
+
+router.get('/:id', peliculasController.listara);
+
+router.get('/1', peliculasController.listara);
+
+module.exports = router;
